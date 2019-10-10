6f08db8a8dda1a9be147d3fe923a5a5e0090692e
saribaev021 <53699219+saribaev021@users.noreply.github.com>
2019-10-10 20:38:06 +0300

    +

--- js/calc.js
+++ js/calc.js
@@ -2,11 +2,11 @@ let select = document.querySelector('input[name = area]:checked').value;
 let formatter = new Intl.NumberFormat("ru");
 let middlePrice = 4850;
 document.querySelector('#floor-area').innerHTML = formatter.format(select)+'м<sup>2</sup';
-document.querySelector('#middle-price_property').innerHTML =formatter.format(+select*middlePrice)+'ТГ';
+document.querySelector('#middle-price_property').innerHTML =formatter.format(+select*middlePrice)+' ТГ';
 document.querySelector('.floor-area').onclick = function(){
   select = document.querySelector('input[name = area]:checked').value;  
   formatter = new Intl.NumberFormat("ru");
   document.querySelector('#floor-area').innerHTML = formatter.format(select)+'м<sup>2</sup';
   middlePrice = 4850;
-  document.querySelector('#middle-price_property').innerHTML =formatter.format(+select*middlePrice)+'ТГ';
+  document.querySelector('#middle-price_property').innerHTML =formatter.format(+select*middlePrice)+' ТГ';
 }

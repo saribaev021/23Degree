9b3c818338c6612ac3d05eb9e5c322bfe0d5d901
saribaev021 <53699219+saribaev021@users.noreply.github.com>
2019-10-16 17:30:36 +0300

    +

--- js/slider.js
+++ js/slider.js
@@ -28,29 +28,96 @@ document.querySelectorAll('.slider').forEach(function(element){
    		 event.stopPropagation();event.stopPropagation();
    		}
 });
-let slideImages = document.querySelectorAll('.slide-image');
-let slideImagesURL = ["img/portfolio/1.png", "img/portfolio/2.png", "img/portfolio/3.png", "img/portfolio/4.png", "img/portfolio/5.png", "img/portfolio/6.png"];
+let slideImage = document.querySelector('.slide-image');
+let slideImage2 = document.querySelector('.slide-image2');
+let slideImage3 = document.querySelector('.slide-image3');
+let slideImage4 = document.querySelector('.slide-image4');
+let slideImageURL = ["img/portfolio/1.png", "img/portfolio/2.png", "img/portfolio/3.png", "img/portfolio/4.png", "img/portfolio/5.png", "img/portfolio/6.png"];
+let slideImageURL2 = ["img/portfolio/2.png", "img/portfolio/4.png", "img/portfolio/1.png", "img/portfolio/5.png", "img/portfolio/6.png", "img/portfolio/3.png"];
+let slideImageURL3 = ["img/portfolio/4.png", "img/portfolio/1.png", "img/portfolio/5.png", "img/portfolio/4.png", "img/portfolio/2.png", "img/portfolio/3.png"];
+let slideImageURL4 = ["img/portfolio/6.png", "img/portfolio/5.png", "img/portfolio/2.png", "img/portfolio/3.png", "img/portfolio/4.png", "img/portfolio/1.png"];
+let slide = document.querySelectorAll('.slide');
+let slide2 = document.querySelectorAll('.slide2');
+let slide3 = document.querySelectorAll('.slide3');
+let slide4 = document.querySelectorAll('.slide4');
 let currentImageIndex = 0
-for (let i = 0; i < slideImages.length; i++) {
-		slideImages[i].src=slideImagesURL[currentImageIndex];
+for (let i = 0; i < slide.length; i++) {
+	 slide[i].onclick=function(){
+	 	currentImageIndex =i
+	 	slider(currentImageIndex)
 	}
-document.querySelectorAll('.show-prev_slide').forEach(function(element){
-	element.disabled = true;
-	element.style.opacity="0.5"
-	element.onclick = showPrevClick;
-});
-document.querySelectorAll('.show-next_slide').forEach(function(element){
-	element.onclick = showNextClick;
-});
+	slide2[i].onclick=function(){
+	 	currentImageIndex =i
+	 	slider(currentImageIndex)
+	}
+	slide3[i].onclick=function(){
+	 	currentImageIndex =i
+	 	slider(currentImageIndex)
+	}
+	slide4[i].onclick=function(){
+	 	currentImageIndex =i
+	 	slider(currentImageIndex)
+	}
+}
+function slider(currentImageIndex){
+	if(currentImageIndex==0){
+	 		document.querySelectorAll('.show-next_slide').forEach(function(element){
+				element.disabled = false;
+				element.style.opacity="1"
+				element.onclick = showNextClick;
+			});	
+	 		document.querySelectorAll('.show-prev_slide').forEach(function(element){
+	 			element.disabled = true;
+				element.style.opacity="0.5"
+			});
+	    }
+	    else if(currentImageIndex == (slide.length - 1)){
+	    	document.querySelectorAll('.show-prev_slide').forEach(function(element){
+	 			element.disabled = false;
+				element.style.opacity="1"
+				element.onclick = showPrevClick;
+			});
+	    	document.querySelectorAll('.show-next_slide').forEach(function(element){
+				element.disabled = true;
+				element.style.opacity="0.5"
+			});	
+	    }
+	    else{
+	    	document.querySelectorAll('.show-prev_slide').forEach(function(element){
+	 			element.disabled = false;
+				element.style.opacity="1"
+				element.onclick = showPrevClick;
+			});
+			document.querySelectorAll('.show-next_slide').forEach(function(element){
+				element.disabled = false;
+				element.style.opacity="1"
+				element.onclick = showNextClick;
+			});	
+
+	    }
+
+	 	slideImageURL.splice(currentImageIndex, 1, slide[currentImageIndex].getAttribute('src'))
+	 	slideImageURL2.splice(currentImageIndex, 1, slide2[currentImageIndex].getAttribute('src'))
+	 	slideImageURL3.splice(currentImageIndex, 1, slide3[currentImageIndex].getAttribute('src'))
+	 	slideImageURL4.splice(currentImageIndex, 1, slide4[currentImageIndex].getAttribute('src'))
+	 	///////////////////////////////////////////////////////////////////////
+	 	///////////////////////////////////////////////////////////////////////
+		slideImage.src=slideImageURL[currentImageIndex];
+		slideImage2.src=slideImageURL2[currentImageIndex];
+		slideImage3.src=slideImageURL3[currentImageIndex];
+		slideImage4.src=slideImageURL4[currentImageIndex];
+		
+}
 function showPrevClick(){
 	document.querySelectorAll('.show-next_slide').forEach(function(element){
 			element.disabled = false;
 			element.style.opacity="1";
 		});
 	currentImageIndex--;
-		for (let i = 0; i < slideImages.length; i++) {
-			slideImages[i].src=slideImagesURL[currentImageIndex];
-		}
+	slideImage.src=slideImageURL[currentImageIndex];
+	slideImage2.src=slideImageURL2[currentImageIndex];
+	slideImage3.src=slideImageURL3[currentImageIndex];
+	slideImage4.src=slideImageURL4[currentImageIndex];
 		if(currentImageIndex == 0){
 			document.querySelectorAll('.show-prev_slide').forEach(function(element){
 				element.disabled = true;
@@ -64,10 +131,11 @@ function showNextClick(){
 		element.style.opacity="1";
 	});
 	currentImageIndex++;
-	for (let i = 0; i < slideImages.length; i++) {
-		slideImages[i].src=slideImagesURL[currentImageIndex];
-	}
-	if(currentImageIndex==(slideImagesURL.length-1)){
+	slideImage.src=slideImageURL[currentImageIndex];
+	slideImage2.src=slideImageURL2[currentImageIndex];
+	slideImage3.src=slideImageURL3[currentImageIndex];
+	slideImage4.src=slideImageURL4[currentImageIndex];
+	if(currentImageIndex==(slideImageURL.length-1)){
 		document.querySelectorAll('.show-next_slide').forEach(function(element){
 			element.disabled = true;
 			element.style.opacity="0.5";
